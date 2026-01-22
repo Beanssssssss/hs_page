ea6ea74c276076f656039d64662d6dcde6d3b726
JONGBEEN KU <bean4102@naver.com>
2026-01-22 14:13:08 +0900

    merged

--- apps/admin/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminDashboard() {
-  return (
-    <div className="space-y-6">
-      <h1>어드민페이지 메인</h1>
-    </div>
-  );
-}
--- apps/www/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-background">
-      <h1>여기가 랜딩페이지</h1>
-    </div>
-  );
-}
